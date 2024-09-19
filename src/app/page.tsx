f8e901a2cf8ccb06f0b97a8e021f4f37706fc88f
Fabrizzio Vilchez <115495332+Fabrizzio20k@users.noreply.github.com>
2024-09-18 20:43:39 -0500

    Add automatic link generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 import Flowers from "@/components/Flowers/Flowers";
 import Bubbles from "@/components/Bubbles/Bubbles";
 import { useState, useEffect } from "react";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
+import { FaCopy, FaWhatsapp, FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const encodeBase64 = (text: string) => {
   return btoa(unescape(encodeURIComponent(text)));
@@ -19,10 +21,9 @@ const decodeBase64 = (encoded: string) => {
 };
 
 export default function Page() {
-  const [name, setName] = useState("N...");
+  const [name, setName] = useState("(...)");
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const router = useRouter();
 
   useEffect(() => {
     const encodedName = searchParams.get("name");
@@ -34,8 +35,28 @@ export default function Page() {
 
   const handleGenerateLink = () => {
     const encodedName = encodeBase64(name);
+    return `${window.location.origin}${pathname}?name=${encodeURIComponent(
+      encodedName
+    )}`;
+  };
+
+  const handleCopyToClipboard = () => {
+    const link = handleGenerateLink();
+    navigator.clipboard.writeText(link).then(() => {
+      alert("Enlace copiado al portapapeles!");
+    });
+  };
+
+  const handleSendWhatsApp = () => {
+    const link = handleGenerateLink();
+    const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(link)}`;
+    window.open(whatsappUrl, "_blank");
+  };
+
+  const handleUpdateUrl = () => {
+    const encodedName = encodeBase64(name);
     const newUrl = `${pathname}?name=${encodeURIComponent(encodedName)}`;
-    router.push(newUrl);
+    window.history.pushState(null, "", newUrl);
   };
 
   return (
@@ -45,7 +66,7 @@ export default function Page() {
       <Bubbles />
       <div className="absolute top-0 left-0 w-full h-full text-white z-50 font-josephsophia text-5xl">
         {/* Utiliza la variable `name` aquí */}
-        <h1 className="h-full p-8">For u ❤️ {name}</h1>
+        <h1 className="h-full p-8">For you ❤️ {name}</h1>
       </div>
 
       {/* Mostrar el cuadro de edición solo si no hay un query parameter */}
@@ -60,20 +81,43 @@ export default function Page() {
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
-            {/* Botón para generar el link */}
+            {/* Botón para copiar al portapapeles */}
             <button
-              className="px-4 py-2 rounded-lg bg-neon text-black hover:bg-opacity-80 transition duration-300"
-              onClick={handleGenerateLink}
+              className="px-4 py-2 rounded-lg bg-neon w-full hover:bg-opacity-80 transition duration-300 flex justify-center items-center"
+              onClick={handleCopyToClipboard}
             >
-              Generar Link
+              <FaCopy className="mr-2" /> {/* Ícono de copia */}
+              Copiar
             </button>
+            {/* Botón para mandar por WhatsApp */}
+            <button
+              className="px-4 py-2 rounded-lg bg-neon w-full hover:bg-opacity-80 transition duration-300 flex justify-center items-center"
+              onClick={handleSendWhatsApp}
+            >
+              <FaWhatsapp className="mr-2" /> {/* Ícono de WhatsApp */}
+              WhatsApp
+            </button>
+            {/* Botón para actualizar la URL sin recargar */}
+            <button
+              className="px-4 py-2 rounded-lg bg-neon w-full hover:bg-opacity-80 transition duration-300 flex justify-center items-center"
+              onClick={handleUpdateUrl}
+            >
+              Actualizar URL
+            </button>
+            <div className="flex flex-row">
+              <Link href="https://github.com/Fabrizzio20k" className="flex flex-row space-x-1">
+                <h2 className="text-xs text-gray-400">Made with ❤️ by Fabrizzio20k </h2>
+                <FaGithub className="mr-2" />
+              </Link>
+            </div>
           </div>
         </div>
       )}
 
       <style jsx>{`
         .bg-neon {
-          background-color: #00ffab; /* Color neón */
+          background-color: #015952;
+          border: 1px solid #00ffab;
         }
         .focus\:border-neon:focus {
           border-color: #00ffab;
